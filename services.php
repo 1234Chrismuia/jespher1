aed926de952369711ba869ed9e457561bfba5789
JESPHER254 <kiambachris29@gmail.com>
2025-08-13 16:25:54 +0300

    Update services.php

--- services.php
+++ services.php
@@ -34,7 +34,8 @@
             </div>
         </div>
     </section>
-    <?php include 'partials/footer.php'; ?>
-    <script src="assets/scripts.js"></script>
+    <?php include 'footer.php'; ?>
+    <script src="scripts.js"></script>
 </body>
+
 </html>
